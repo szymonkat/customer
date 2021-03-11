5acb217f3b9a84142cbd946c0a2cf33306232297
szymonkat <szymonkat@gmail.com>
2021-03-11 15:41:36 +0100

    Revert "Ribbon test"
    This reverts commit 3e2f70e7371d5fccfda63480676a289493295443.

--- src/main/java/com/kodilla/customer/connector/AccountsConnector.java
+++ src/main/java/com/kodilla/customer/connector/AccountsConnector.java
@@ -1,9 +1,7 @@
 package com.kodilla.customer.connector;
 
-import com.kodilla.customer.configuration.RibbonConfiguration;
 import com.kodilla.customer.domain.GetAccountsResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collections;
 
-@RibbonClient(name = "accounts", configuration = RibbonConfiguration.class)
 @FeignClient(name = "accounts", fallback = AccountsConnector.AccountsConnectorFallback.class)
 public interface AccountsConnector {
 
