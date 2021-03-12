c20aa20ff3d30f61e7c461084af79b4564f55ab1
szymonkat <szymonkat@gmail.com>
2021-03-12 19:55:56 +0100

    tests_run_attempt

---
+++ src/test/java/com/kodilla/customer/service/interfaces/ProductServiceTest.java
@@ -0,0 +1,43 @@
+package com.kodilla.customer.service.interfaces;
+
+import com.kodilla.customer.dto.AccountDto;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureStubRunner(
+        stubsMode = StubRunnerProperties.StubsMode.LOCAL,
+        ids = {
+                "com.kodilla:accounts:+:stubs:9000"
+        }
+)
+class ProductServiceTest {
+
+    @Autowired
+    private ProductService productService;
+
+
+    @Test
+    void findCustomerAccounts() {
+        //Given
+        Long customerId = 1L;
+
+        //When
+        List<AccountDto> accounts = productService.findCustomerAccounts(customerId);
+
+        //Then
+        assertEquals(1, accounts.size());
+    }
+
+}
